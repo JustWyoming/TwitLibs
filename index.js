2cb57e1eebdbe28169ce3144aa232b852d13d98a
JustWyoming <wyomingt@gmail.com>
2015-01-02 13:09:38 -0800

    added 404 page, cleaned up indents, file org

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(session({
 
 app.use(flash());
 
+
+
 //index routes//
 app.get("/", function(req, res){ 
   res.render('index',{template:false, selected: 1});
@@ -28,31 +30,31 @@ app.get("/", function(req, res){
 
 app.post('/', function(req, res){
  db.template.find({where: {category_id: req.body.libId}}).then(function(libTemplate){
-    helpers.doLib(libTemplate.content,req.body.hash,function(libbed) {
-       var selected = req.body.libId
-      res.render('index',{template: libbed, libId: libTemplate.id, selected: selected})
-    })   
-    
-  }) 
+  helpers.doLib(libTemplate.content,req.body.hash,function(libbed) {
+   var selected = req.body.libId
+   res.render('index',{template: libbed, libId: libTemplate.id, selected: selected})
+ })   
+
+}) 
 
 })
  //fame save routes
  app.post('/fame', function(req, res){
- db.save.create(req.body).then(function(data){
-        res.redirect('/fame/'+data.id);
-      })
-  
-     
+   db.save.create(req.body).then(function(data){
+    res.redirect('/fame/'+data.id);
+  })
+
+
  })
 
-app.get("/fame", function(req, res){ 
+ app.get("/fame", function(req, res){ 
   db.save.findAll().done(function(error,data){
     res.render('fame',{data:data});
   })
   
 })
 
-app.get("/fame/:id",function(req,res){
+ app.get("/fame/:id",function(req,res){
   db.save.find(req.params.id).done(function(error,data){
     var shareUrl = encodeURIComponent("http://"+req.headers.host+"/fame/"+data.id);
     var picUrl = encodeURIComponent("http://"+req.headers.host+"/screengrab.png");
@@ -68,6 +70,15 @@ app.get("/rules", function(req, res){
 })
 
 
+// about routes
+app.get("/about", function(req, res){ 
+  res.render('about');
+})
+
+//error page
+app.use(function(req, res, next){
+  res.render('404');
+});
 
 
 
---
+++ insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO categorys VALUES (1,'Work', now(), now(), now(), now());
+INSERT INTO categorys VALUES (2,'Family', now(), now());
+INSERT INTO categorys VALUES (3,'Celebrity', now(), now());
+INSERT INTO categorys VALUES (4,'Activity', now(), now());
+INSERT INTO categorys VALUES (5,'Dream', now(), now());
+INSERT INTO categorys VALUES (6,'Travel', now(), now());
+
+INSERT INTO templates VALUES (1, 1,'It was a [[JJ]], rainy Monday. Traffic was totally [[JJ]], and really [[VBD]] my vibe. [[NNPS]] was on the radio, adding to the [[NN]]. After a [[VBG]] commute, I arrived at work to find my boss [[searchTerm]] a [[NN]]. I guess it’s too late to call in [[JJ]].', now(), now());
+
+INSERT INTO templates VALUES (2, 2,'Last night was freaking [[JJ]]. My [[searchTerm]] and I were [[VBG]] through a massive pile of [[NN]]. Naturally things escalated quickly, and before long [[NNS]] we were climbing all over the [[JJ]]. This is why we can’t have anything [[JJ]].', now(), now());
+
+INSERT INTO templates VALUES (3, 3,'My friend James is in love with [[searchTerm]]. Seriously, he’s [[JJ]] and probably needs to be [[VBD]]. Yesterday at [[NN]], he announced he’s been sending [[NN]] on the regular. It’s totally affecting his relationship with [[NN]], and now [[NNPS]] are starting to talk. ', now(), now());
+
+INSERT INTO templates VALUES (4, 4,'[[searchTerm]] is incredible. The [[JJ]] you get when you [[VB]] [[NN]] is unreal. It’s [[RB]], which makes it great for [[VBG]] with family and friends. Personally, I recommend trying [[NN]] for [[JJS]] results.', now(), now());
+
+INSERT INTO templates VALUES (5, 5,'Last night I had the [[JJS]] dream. I was in [[searchTerm]] with [[NN]]. Everything seemed normal until [[NNS]] started [[VBG]]. People began to [[NN]] and suddenly a [[NN]] appeared out of [[JJ]]. When I woke up I was [[JJ]].', now(), now());
+
+INSERT INTO templates VALUES (6, 6,'My dad just bought a ticket for [[searchTerm]]. He’s the [[JJS]]. He really thinks he’ll meet [[NNPS]] and party like  [[NN]]. He also thinks he’ll [[VB]] and [[VB]] until [[JJ]]. I don’t want to kill his hopes and dreams, but I think he’s more likely to find [[NN]].', now(), now());
+
+INSERT INTO saves VALUES (1, 1,'It was a SWEATY, rainy Monday. Traffic was full of SINGLE CHARLENES reacting BASICALLY. The radio was playing BJORK, which really made me HORNY. After a BLAZING commute, I finally arrived at work to find my boss ADDICTING a FORK. I guess it’s too late to call in DEAD.', now(), now());
+
+INSERT INTO saves VALUES (2, 1,'It was a FURRY, rainy Monday. Traffic was totally WEAK, and really SLOW my vibe. BROGRAMMERS were on the radio, adding to the BADSANTAALAD. After a FARTING commute, I arrived at work to find my boss FINGERING a FANNY. I guess it’s too late to call in SICK.', now(), now());
+
+INSERT INTO saves VALUES (3, 6,'My dad just bought a ticket for PAPA NEW GUINEA. He’s the HUNGRIEST. He really thinks he’ll meet EUROPEANS and party like  URMINDBLOWN. He also thinks he’ll FUCK and PRONOUNCE until CLEAR. I don’t want to kill his hopes and dreams, but I think he’s more likely to find WATER.', now(), now());
